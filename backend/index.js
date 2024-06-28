a85c13c678bfce79a4e54ef8e6768b282727df67
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2024-06-28 22:38:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ import cors from 'cors';
 import { java } from './compiler.js';
 const app = express();
 
-app.use(cors());
+app.use(cors({
+        origin: ["https://compilerpro.vercel.app/"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }));
 app.use(express.json());
 
 
